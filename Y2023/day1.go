950fb51797938cdc4758a15bc6ecc35a0caa3810
Sujitha Wijewantha <sujithacrysis@gmail.com>
2024-08-15 19:19:42 +0530

    wp day 1 part 2

--- Y2023/day1.go
+++ Y2023/day1.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -25,16 +26,38 @@ func main() {
 		var length = len(line)
 		var number = ""
 		var lastNumber = ""
+		var potentialTextNumber = ""
+		var lastNumberIndex = -1
+		var lastPotentialTextNumber = ""
+		var lastPotentialTextNumberIndex = -1
+
 		for i := 0; i < length; i++ {
 			if _, err := strconv.Atoi(string(line[i])); err == nil {
 				lastNumber = string(line[i])
+				lastNumberIndex = i
 				if len(number) == 0 {
+					// First number found
 					number += lastNumber
+				}
+			} else {
+				potentialTextNumber += string(line[i])
+				if num, isNumber := textToNumber(potentialTextNumber); isNumber {
+					lastPotentialTextNumberIndex = i
+					lastPotentialTextNumber = strconv.Itoa(num)
+					potentialTextNumber = ""
+					if len(number) == 0 {
+						number += strconv.Itoa(num)
+					}
 				}
 			}
 			// Once we have reached the end of the line, add the last number to the number
 			if i == length-1 {
-				number += lastNumber
+				if lastNumberIndex > lastPotentialTextNumberIndex {
+					number += lastNumber
+				}
+				if lastPotentialTextNumberIndex > lastNumberIndex {
+					number += lastPotentialTextNumber
+				}
 			}
 		}
 
@@ -44,4 +67,26 @@ func main() {
 	}
 
 	fmt.Println("Total: ", total)
+}
+
+func textToNumber(ch string) (number int, isNumber bool) {
+	numberMap := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	for key, value := range numberMap {
+		if strings.Contains(ch, key) {
+			return value, true
+		}
+	}
+
+	return 0, false
 }
